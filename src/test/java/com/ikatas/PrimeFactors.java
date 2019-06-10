1e837fbd5346196926742b7e7c863d93e6a1d9ca
selonj <selonj@163.com>
2019-06-10 21:14:25 +0800

    GREEN: make the test pass by adding another if condition

--- src/test/java/com/ikatas/PrimeFactors.java
+++ src/test/java/com/ikatas/PrimeFactors.java
@@ -10,6 +10,10 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> of(int n) {
         List<Integer> primes = new ArrayList<>();
+        if (n % 2 == 0) {
+            primes.add(2);
+            n /= 2;
+        }
         if (n > 1) {
             primes.add(n);
         }
