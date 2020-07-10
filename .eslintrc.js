5d767b3ca2fa576037693789ca523ec6097b4ded
i-katas <feexonj@gmail.com>
2020-07-10 20:05:36 +0800

    nodejs

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "jest": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
+
---
+++ test/prime-factors.test.js
@@ -0,0 +1,27 @@
+import factorsOf from 'prime-factors'
+
+describe('prime factors', () => {
+  it('no factors', () => {
+    expect(factorsOf(1)).toEqual([])
+  })
+
+  it('prime numbers', () => {
+    expect(factorsOf(2)).toEqual([2])
+    expect(factorsOf(3)).toEqual([3])
+  })
+
+  it('composite number that containing same factors', () => {
+    expect(factorsOf(4)).toEqual([2, 2])
+    expect(factorsOf(8)).toEqual([2, 2, 2])
+    expect(factorsOf(9)).toEqual([3, 3])
+  })
+
+  it('composite number that containing diff factors', () => {
+    expect(factorsOf(6)).toEqual([2, 3])
+  })
+
+  it('big prime number', () => {
+    let value = 0x7fffffff
+    expect(factorsOf(value)).toEqual([value])
+  })
+})
